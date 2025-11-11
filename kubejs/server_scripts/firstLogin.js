b445ba81b45cbe8e1fb6d94cd39c226d302c355c
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-11-11 07:37:43 -0600

    typo

--- kubejs/server_scripts/firstLogin.js
+++ kubejs/server_scripts/firstLogin.js
@@ -2,7 +2,7 @@ PlayerEvents.loggedIn(e => {
     if (!e.hasGameStage('started')) {
         e.player.give('splendid_slimes:slime_vac')
         e.player.give('ftbquests:book')
-        e.player.give('numismatic_utils:bank_meter')
+        e.player.give('numismatics_utils:bank_meter')
 
         e.addGameStage('started')
     }
