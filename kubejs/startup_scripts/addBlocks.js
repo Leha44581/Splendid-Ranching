e996a60a873ae949ab06aa362a8f5212b26ea89c
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-11-24 22:59:24 -0600

    update wares block and model

--- kubejs/startup_scripts/addBlocks.js
+++ kubejs/startup_scripts/addBlocks.js
@@ -1,6 +1,15 @@
-StartupEvents.registry('block', event => {
-    event.create('splendid_wares_shop')
-        .displayName('Splendid Wares Shop')
-        .tagBlock('minecraft:mineable/pickaxe')
-        .soundType('stone')
+StartupEvents.registry('block', e => {
+	// splendid wares shop terminal block 
+	/** @type {Internal.BasicBlockJS$Builder} */
+	let wares = e.create('splendid_wares_shop')
+		.displayName('Splendid Wares Shop')
+		.hardness(30)
+		.tagBlock('minecraft:mineable/pickaxe')
+		.soundType('netherite_block')
+		.model('numismatics:block/creative_display_case')
+		.box(0, 0, 0, 16, 8, 16)
+		.box(1, 8, 1, 15, 18, 15)
+		.defaultCutout()
+
+
 })
